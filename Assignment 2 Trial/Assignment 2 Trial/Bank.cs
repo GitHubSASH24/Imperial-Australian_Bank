524240a0e61725788b857627f300b90f38d64c7c
evabxbxb <164961771+evabxbxb@users.noreply.github.com>
2024-04-02 15:39:32 +1100

    Add files via upload

---
+++ Assignment 2 Trial/Assignment 2 Trial/Bank.cs
@@ -0,0 +1,13 @@
+﻿
+internal class Bank
+{
+    internal bool Login(string? usernameLogin, string? passwordLogin)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal bool SignUp(string? usernameSignUp, string? passwordSignUp)
+    {
+        throw new NotImplementedException();
+    }
+}
